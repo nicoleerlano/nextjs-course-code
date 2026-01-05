39abff278a9edc5933c7d06057be580ce8121649
nicoleerlano <erlanonicole98@gmail.com>
2026-01-05 23:51:31 +0200

    project layout

---
+++ pages/posts/[slug].js
@@ -0,0 +1,8 @@
+export default function PostDetailPage() {
+  return (
+    <div>
+      <h1>Post Detail Page</h1>
+      <p>This page shows the details of a specific post based on the slug.</p>
+    </div>
+  );
+}
---
+++ pages/posts/index.js
@@ -0,0 +1,8 @@
+export default function AllPostsPage() {
+  return (
+    <div>
+      <h1>All Posts</h1>
+      <p>Here is a list of all blog posts.</p>
+    </div>
+  );
+}

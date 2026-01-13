4aa4aa27b66636157b827bee52844f93a4acf06d
nicoleerlano <erlanonicole98@gmail.com>
2026-01-13 23:45:12 +0200

    featured posts & all posts components

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -12,12 +12,19 @@ export default function PostItem(props) {
   });
 
   const imagePath = `/images/posts/${slug}/${image}`;
+  const linkPath = `/posts/${slug}`;
 
   return (
-    <li>
-      <Link>
+    <li className={classes.post}>
+      <Link href={linkPath}>
         <div className={classes.image}>
-          <Image src={imagePath} alt={title} width={300} height={200} />
+          <Image
+            src={imagePath}
+            alt={title}
+            width={300}
+            height={200}
+            layout="responsive"
+          />
         </div>
         <div className={classes.content}>
           <h3>{title}</h3>
